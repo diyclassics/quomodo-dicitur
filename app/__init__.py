4867c0a3238d14ad8b67bba6d7afadf9155a11c9
Patrick J. Burns <patrick@diyclassics.org>
2018-07-18 10:27:07 -0400

    Add bootstrap, update templates

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
