01d2f2da68e38e82979cd1d55a414a08a35b1c92
Patrick J. Burns <patrick@diyclassics.org>
2018-07-19 16:15:06 -0400

    Update time in templates

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
